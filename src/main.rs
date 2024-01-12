552d93a01570aeb2217a6ece2d97104ea1a4defe
Michal Janočko <33790048+michaljanocko@users.noreply.github.com>
2024-01-12 17:44:47 +0100

    ✏️ rate → rates

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tokio::spawn(data::schedule_dataset_update(dataset.clone()));
 
     let service =
-        OpenApiService::new(api::Api, "Exchange rate API", "1.0").server("https://exchange.rates");
+        OpenApiService::new(api::Api, "Exchange rates API", "1.0").server("https://exchange.rates");
 
     let app = Route::new().nest("/", service.data(dataset));
 
